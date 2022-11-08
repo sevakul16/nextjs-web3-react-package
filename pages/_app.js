d30c9d479ad24481c49ab4b7df6f49ca023dbdd0
vsevolod.kulev <vsevolods.kuvevs@bertelsmann.de>
2022-11-08 13:15:58 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { Web3Provider } from "@ethersproject/providers";
+import { Web3ReactProvider } from "@web3-react/core";
+import "../styles/globals.css";
+
+const getLibary = (provider) => {
+  return new Web3Provider(provider);
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
